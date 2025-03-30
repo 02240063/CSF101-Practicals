b861b57763eff17f98c1f5067065b1639bd9b0d1
ChimiRinzin <02240063.cst@rub.edu.bt>
2025-03-30 19:10:31 +0700

    my second commit

---
+++ implemention of basic sum.py
@@ -0,0 +1,6 @@
+print("enter any first number ")
+n1 = int(input())
+print("enter any second number ")
+n2 = int(input())
+sum = n1 + n2
+print("the sum of two numbers are : " + str(sum))
---
+++ practical0.py
@@ -0,0 +1,4 @@
+studentName = "pema"
+studentNumber = "02240122"
+
+print("My name is " + studentName + " and my student number is " + studentNumber)
